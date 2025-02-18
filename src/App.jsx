150cf83b0413d1ec05f3f19677c710a7a62ba4a0
Zahra Mehri <zahra.m@4D.local>
2025-02-18 14:35:51 +0330

    API

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { useState } from 'react'
 import Todos from './components/Todos'
 
 
+
 function App() {
 
   return (
--- src/components/Todos.jsx
+++ src/components/Todos.jsx
@@ -2,80 +2,147 @@ import { useEffect, useState } from "react";
 import TodoList from "./todoList";
 import { v4 as uuidv4 } from 'uuid';
 import NewTodoInput from "./NewTodoInput";
+import { toast } from "react-toastify";
 
 
 export default function Todos() {
 
     const [todos, setTodos] = useState([]);
 
-    const addNewTodoHandler = (newTodoTitle) => {
+    const addNewTodoHandler = async (newTodoTitle) => {
 
-        setTodos(
-            [
-                ...todos,
-                {
-                    id: uuidv4(),
+        try {
+
+            let res = await fetch('https://67b30741bc0165def8cf9bd0.mockapi.io/todos', {
+                method: 'POST',
+                headers: { 'content-type': 'application/json' },
+                body: JSON.stringify({
                     title: newTodoTitle,
                     status: false
+                })
+            })
+
+            let todoData = await res.json();
+            setTodos(
+                [
+                    ...todos,
+                    todoData
+                ]
+            )
+            console.log(todos);
+            toast.success('todo created !')
+
+        } catch (error) {
+            console.error('Fetch error:', error);
+        }
+
+    };
+
+    const deleteTodoHandler = async (todo) => {
+
+        let res = await fetch(`https://67b30741bc0165def8cf9bd0.mockapi.io/todos/${todo.id}`, {
+            method: 'DELETE'
+        })
+
+        if (res.ok) {
+            let newTodos = todos.filter((todoItem) => todoItem.id !== todo.id);
+            setTodos(newTodos);
+            toast.success('todo deleted successfully !')
+
+        } else {
+            let message = await res.json();
+            toast.error(message);
+        }
+
+    };
+
+
+    const toggleTodoStatusHandler = async (todo) => {
+
+        const res = await fetch(`https://67b30741bc0165def8cf9bd0.mockapi.io/todos/${todo.id}`, {
+            method: 'PUT',
+            headers: { 'content-type': 'application/json' },
+            body: JSON.stringify({ status: !todo.status })
+        })
+
+        if (res.ok) {
+
+            let newTodos = todos.map((todoItem) => {
+
+                if (todoItem.id == todo.id) {
+
+                    todoItem.status = !todoItem.status;
+
                 }
-            ]
-        )
+                return todoItem;
+            })
 
-    };
+            setTodos(newTodos);
 
-    const deleteTodoHandler = (todo) => {
+        } else {
+            let message = await res.json();
+            toast.error(message);
+        }
 
-        let newTodos = todos.filter((todoItem) => {
-            return todoItem.id !== todo.id;
-        })
-        setTodos(newTodos);
-    };
-
-
-    const toggleTodoStatusHandler = (todo) => {
-
-        let changeTodo = todos.map((todoItem) => {
-
-            if (todoItem.id == todo.id) {
-
-                todoItem.status = !todoItem.status;
-
-            }
-            return todoItem;
-
-        })
-
-        setTodos(changeTodo);
     }
 
 
-    const editTodoTitleHandler = (todo, newTitle) => {
+    const editTodoTitleHandler = async (todo, newTitle) => {
 
-        let newTodo = todos.map((todoItem) => {
-
-            if (todoItem.id == todo.id) {
-
-                todoItem.title = newTitle;
-
-            }
-            return todoItem;
-
+        const res = await fetch(`https://67b30741bc0165def8cf9bd0.mockapi.io/todos/${todo.id}`, {
+            method: 'PUT',
+            headers: { 'content-type': 'application/json' },
+            body: JSON.stringify({ title: newTitle })
         })
 
-        setTodos(newTodo);
+        if (res.ok) {
+
+            let newTodo = todos.map((todoItem) => {
+
+                if (todoItem.id == todo.id) {
+
+                    todoItem.title = newTitle;
+
+                }
+                return todoItem;
+
+            })
+
+            setTodos(newTodo);
+
+        } else {
+            let message = await res.json();
+            toast.error(message);
+        }
+
+
+    }
+
+    const getTodosFromApi = async () => {
+
+        try {
+
+            let res = await fetch('https://67b30741bc0165def8cf9bd0.mockapi.io/todos')
+            let Todos = await res.json();
+
+            if (res.ok) {
+                setTodos(Todos)
+            }
+
+            //show eror
+
+        } catch (error) {
+            console.log(error);
+
+        }
+
     }
 
     useEffect(() => {
-        setTodos(JSON.parse(localStorage.getItem('todos_list')) ?? [])
+
+        getTodosFromApi();
 
     }, [])
-
-    useEffect(() => {
-        localStorage.setItem('todos_list', JSON.stringify(todos));
-        console.log(localStorage.getItem('todos_list'));
-
-    }, [todos])
-
 
     return (
 
